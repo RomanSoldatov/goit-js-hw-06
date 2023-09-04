fe8e5811de90db1af4f2f28697dd6821b2597f3f
Roman <roman.soldatov13@gmail.com>
2023-09-04 20:30:17 +0300

    try 10
    add 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const divEl = document.querySelector("#controls");
+const btnCreateEl = document.querySelector("[data-create]");
+const btnCloseEl = document.querySelector("[data-destroy]");
+const divBoxesEL = document.querySelector("#boxes");
+
+btnCreateEl.addEventListener("click", createBoxes);
+btnCloseEl.addEventListener("click", destroyBoxes);
+
+let i = 1;
+let width = 30;
+let height = 30;
+function createBoxes() {
+  while (i <= divEl.firstElementChild.value) {
+    const divEl = document.createElement("div");
+    divEl.style.width = `${width}px`;
+    divEl.style.height = `${height}px`;
+    divEl.style.backgroundColor = `${getRandomHexColor()}`;
+
+    divBoxesEL.appendChild(divEl);
+
+    i += 1;
+    width += 10;
+    height += 10;
+  }
+}
+function destroyBoxes() {
+  divBoxesEL.innerHTML = "";
+  document.location.reload();
+}
