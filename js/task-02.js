70e70a2db8df2bda4eea7d8f75646e9494361ad6
Roman <roman.soldatov13@gmail.com>
2023-09-04 11:36:10 +0300

    try-2
    add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const newIngredients = [];
+const ingredientsEL = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  let newIngridient = document.createElement("li");
+  newIngridient.classList.add("item");
+  newIngridient.textContent = ingredient;
+  newIngredients.push(newIngridient);
+  return;
+});
+ingredientsEL.append(...newIngredients);
