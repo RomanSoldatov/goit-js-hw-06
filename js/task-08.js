47f24ad4fde16706232ab5b3224e28bb14ca4c7d
Roman <roman.soldatov13@gmail.com>
2023-09-04 19:13:06 +0300

    try 8
    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Поля повинні бути заповнені всі!");
+  } else {
+    const Data = {
+      email: email.value,
+      password: password.value,
+    };
+
+    console.log(Data);
+    event.currentTarget.reset();
+  }
+}
