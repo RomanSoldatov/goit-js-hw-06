74bf7e8be951a85c99b369951f659525cfaee8b2
Roman <roman.soldatov13@gmail.com>
2023-09-04 14:42:59 +0300

    try-4
    add task 4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const valueEl = document.querySelector("#value");
+
+let counterValue = 0;
+valueEl.textContent = counterValue;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+});
